2d0f6876be542f8df0b9562a82cb1fb3316273a7
Arpit Agrawal <agrawal.arpit03@nmims.edu.in>
2025-06-15 19:40:06 +0200

    exception.py and how to raise an exception

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ sys.path.append(os.path.join(os.getcwd(), "src"))
 
 # Now you can import logger
 from mlproject.logger import logging
+from mlproject.exceptions import CustomException
 
 logging.info("Logging is working from app.py!")
+try:
+    # Simulating an error to demonstrate CustomException
+    a=1/0
+except Exception as e:
+    logging.info("CustomException has been raised and caught successfully!")
+    raise CustomException(e, sys) from e
